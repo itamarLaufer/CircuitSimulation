a21f9a44d51413a4cb5842d91cc5aa554b4fa0c2
Itamar Laufer <35191767+itamarLaufer@users.noreply.github.com>
2019-02-16 19:40:32 +0200

    Update Resistor.java

--- Resistor.java
+++ Resistor.java
@@ -1,7 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 /**
  * this class represents a resistor in a circuit
@@ -12,39 +11,23 @@ public class Resistor
     private double resistance; //Ω
     private double current; //A
     private double voltage; //V
+    LinkedList<Character> id;
+    BinNode<Resistor>cut;
     /**
-     * resistor1 and 2 are the resistors which are connected to the resisotr in 1 side the resistor is probably connect to at least one more resistor in the other side
+     * resistor1 and 2 are the resistors which are connected to the resistor in 1 side the resistor is probably connect to at least one more resistor in the other side
      */
-    protected Resistor resistor1;
-    protected Resistor resistor2;
 
     /**
-     * constructor metod creates Resistor with the given parameters
+     * constructor method creates Resistor
      * @param name the name of the resistor
-     * @param resistance the resistence of the resistor in Ω
+     * @param resistance the resistance of the resistor in Ω
      */
     public Resistor(String name,double resistance) {
         this.name=name;
         this.resistance = resistance;
         current=0;
         voltage=0;
-        resistor1=null; //no resistor is connected
-        resistor2=null;
     }
-
-    /**
-     * constructor metod creates Resistor with the given parameters
-     * @param name the name of the resistor
-     * @param resistance the resistence of the resistor in Ω
-     * @param resistor1 a resistor which is connected to the resistor that the constructor builds (null means nothing is connected)
-     * @param resistor2 a resistor which is connected to the resistor that the constructor builds (null means nothing is connected) if resistor1 is null it must be also null
-     */
-    public Resistor(String name,double resistance,Resistor resistor1,Resistor resistor2) {
-        this(name,resistance);
-        this.resistor1=resistor1;
-        this.resistor2=resistor2;
-    }
-
 
     public double getResistance() {
         return resistance;
@@ -52,7 +35,7 @@ public class Resistor
 
     /**
      *
-     * @return the current in Ampers which pass in the resistor (this data is  filled after this resistor is associated with circuit)
+     * @return the current in Ampere which pass in the resistor (this data is  filled after this resistor is associated with circuit)
      */
     public double getCurrent() {
         return current;
@@ -60,7 +43,7 @@ public class Resistor
 
     /**
      *
-     * @return the voltage on the resistor in Voltes (this data is  filled after this resistor is associated with circuit)
+     * @return the voltage on the resistor in Volt (this data is  filled after this resistor is associated with circuit)
      */
     public double getVoltage() {
         return voltage;
@@ -71,11 +54,11 @@ public class Resistor
     }
 
     /**
-     * method calculates the voltage on the resistor after it's associated to a circuit using Owm Law  V =I*R
+     * method calculates the voltage on the resistor after it's associated to a circuit using Owm Law  V = I*R
      */
     public void calculateVoltage()
     {
-        this.voltage=current*resistance;
+        this.voltage = current*resistance;
     }
 
     public void setResistance(double resistance) {
@@ -89,32 +72,13 @@ public class Resistor
                 ", resistance=" + resistance +"Ω"+
                 ", current=" + current +"A"+
                 ", voltage=" + voltage +"Ω"+
-                ", resistor1=" + resistor1 +
-                ", resistor2=" + resistor2 +
                 '}';
     }
 
-    /**
-     *
-     * @return whether this resistor is connected to 2 resistors (after it)
-     */
-    public boolean split()
-    {
-        return !(resistor1==null||resistor2==null);
-    }
 
     /**
      *
-     * @return whether this resistor is not connected (after it) to any resistor (it means that it's connected to the voltage source)
-     */
-    public boolean isFinal()
-    {
-        return (resistor1==null&&resistor2==null);
-    }
-
-    /**
-     *
-     * @return the power in Wats thich is achived in the resistor using p = I*V
+     * @return the power in Wat that is achieved in the resistor using p = I*V
      */
     public double getPower()
     {
@@ -129,16 +93,5 @@ public class Resistor
         this.name = name;
     }
 
-    /**
-     * the method connects the given resistor this resistor if possible (it's not already connected to 2 resistors)
-     * @param other resistor to connect
-     */
-    public void connect(Resistor other)
-    {
-        if(resistor1==null)
-            resistor1=other;
-        else if(resistor2==null)
-            resistor2=other;
-    }
 
 }
