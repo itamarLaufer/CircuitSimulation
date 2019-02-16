aefc7d67492f2ab59d14f6104b621773d13dee6c
Itamar Laufer <35191767+itamarLaufer@users.noreply.github.com>
2019-02-16 19:38:13 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,9 +7,36 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner in = new Scanner(System.in);
-    static List<Resistor> resistors;
+    static List<BinNode<Resistor>> resistors;
     public static void main(String[] args) {
-        System.out.println(findResistorByName("r1"));
+        resistors = new ArrayList<>(10);
+        BinNode<Resistor> positiveTrigger = new BinNode<>(new Resistor("positiveTrigger",0));
+        BinNode<Resistor> negativeTrigger = new BinNode<>(new Resistor("negativeTrigger",0));
+        BinNode<Resistor> r1 = new BinNode<>(new Resistor("r1" ,10));
+        BinNode<Resistor> r2 = new BinNode<>(new Resistor("r2" ,8));
+        BinNode<Resistor> r3 = new BinNode<>(new Resistor("r3" ,5));
+        BinNode<Resistor> r4 = new BinNode<>(new Resistor("r4" ,20));
+        positiveTrigger.connect(r1);
+        r1.connect(r2);
+        r1.connect(r4);
+        r2.connect(r3);
+        r3.connect(negativeTrigger);
+        r4.connect(negativeTrigger);
+        resistors.add(r1);
+        resistors.add(r2);
+        resistors.add(r3);
+        resistors.add(r4);
+        resistors.add(positiveTrigger);
+        resistors.add(negativeTrigger);
+        Circuit circuit = new Circuit(20, positiveTrigger, negativeTrigger, resistors);
+        for(BinNode<Resistor> r: resistors){
+            System.out.println(r);
+        }
+
+
+
+
+        /*
         ResistorFactory factory = new ResistorFactory();
         System.out.println("In our system you can create circuits and view the the data about the components");
         System.out.println("In our system every component can be connected to maximum 2 other components.s");
@@ -28,6 +54,8 @@ public class Main {
         Resistor r2;
         Resistor positiveTrigger = new Resistor("positiveTrigger",0);
         Resistor negativeTrigger = new Resistor("negativeTrigger",0);
+        resistors.add(positiveTrigger);
+        resistors.add(negativeTrigger);
         while(true)
         {
             System.out.println("Insert the name of the resistor that you want to connect it with other (Don't connect both sides! There is a meaning to the order!)");
@@ -41,14 +69,13 @@ public class Main {
         }
         System.out.println("Insert emp of voltage source (V)");
         Circuit circuit = new Circuit(in.nextDouble(),positiveTrigger,negativeTrigger,resistors);
-        circuit.toString();
+        circuit.toString(); */
     }
-    @Nullable
-    public static Resistor findResistorByName(String name)
+    public static BinNode<Resistor> findResistorByName(String name)
     {
-        for(Resistor resistor:resistors)
+        for(BinNode<Resistor> resistor:resistors)
         {
-            if(resistor.getName().equals(name))
+            if(resistor.getValue().getName().equals(name))
                 return resistor;
         }
         return null;
