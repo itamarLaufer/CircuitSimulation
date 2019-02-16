d1baae03d4da1932f7c9c80d8457eaa091b0b293
Itamar Laufer <35191767+itamarLaufer@users.noreply.github.com>
2019-02-16 19:36:31 +0200

    Update Amperometer.java

--- Amperometer.java
+++ Amperometer.java
@@ -12,19 +12,6 @@ public class Amperometer extends Resistor implements Meter {
         super(name,0);
     }
 
-    /**
-     * creates aa amperometer with the given data
-     * @param name the name of the amperometer
-     * @param resistor1 a resistor which is connected to the amperometer that the constructor builds (null means nothing is connected)
-     * @param resistor2 a resistor which is connected to the amperometer that the constructor builds (null means nothing is connected) if resistor1 is null it must be also null
-     */
-    public Amperometer(String name,Resistor resistor1, Resistor resistor2) {
-        super(name,0, resistor1, resistor2);
-    }
-    /**
-     * mehod gives the value shown on the amperometer display
-     * @return the current that flow through it
-     */
     @Override
     public double getInfo() {
         return getCurrent();
